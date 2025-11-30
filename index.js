8642080ee2afc2dca4e6b97a7d9c178985fe848e
Marvel-tech-hub <marvelscout2004@gmail.com>
2025-11-30 02:33:27 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function toggle_menu(){
+    const menu = document.querySelector('.menu_links');
+    const icon = document.querySelector('.handburger_icon');
+    menu.classList.toggle('open')
+    icon.classList.toggle('open')
+}
